090a6f21899a83aa58adc46d4eccdc619f9fd57f
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-09-08 00:48:03 -0500

    completed add user component

--- workout-tracker-app/src/components/create-exercise.component.js
+++ workout-tracker-app/src/components/create-exercise.component.js
@@ -32,7 +32,7 @@ export default class CreateExercise extends Component {
 
   componentDidMount() {
     axios
-      .get("http://localhost:5000/users/")
+      .get("http://localhost:3000/users/")
       .then((response) => {
         if (response.data.length > 0) {
           this.setState({
@@ -111,7 +111,7 @@ export default class CreateExercise extends Component {
     console.log(exercise);
 
     axios
-      .post("http://localhost:5000/exercises/add", exercise)
+      .post("http://localhost:3000/exercises/add", exercise)
       .then((res) => console.log(res.data));
 
     window.location = "/";
@@ -120,7 +120,7 @@ export default class CreateExercise extends Component {
   render() {
     return (
       <div>
-        <h3>Create New Exercise Log</h3>
+        <h3>Add A Workout</h3>
         <form onSubmit={this.onSubmit}>
           <div className="form-group">
             <label>Username: </label>
