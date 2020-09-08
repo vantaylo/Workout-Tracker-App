ad23c4806c356df55022eb1969b2c5b157ce0f85
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-09-08 11:12:44 -0500

    completed exercise list and edit components. Reformatted folder structure

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,215 @@
+import React, { Component } from "react";
+import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+export default class EditExercise extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeType = this.onChangeType.bind(this);
+    this.onChangeWeight = this.onChangeWeight.bind(this);
+    this.onChangeSets = this.onChangeSets.bind(this);
+    this.onChangeReps = this.onChangeReps.bind(this);
+    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeDistance = this.onChangeDistance.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: "",
+      type: "",
+      weight: 0,
+      sets: 0,
+      reps: 0,
+      duration: 0,
+      distance: 0,
+      date: new Date(),
+      users: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3000/exercises/" + this.props.match.params.id)
+      .then((response) => {
+        this.setState({
+          username: response.data.username,
+          type: response.data.type,
+          weight: response.data.weight,
+          sets: response.data.sets,
+          reps: response.data.reps,
+          duration: response.data.duration,
+          distance: response.data.distance,
+          date: new Date(response.data.date),
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    axios
+      .get("http://localhost:3000/users/")
+      .then((response) => {
+        if (response.data.length > 0) {
+          this.setState({
+            users: response.data.map((user) => user.username),
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+  onChangeType(e) {
+    this.setState({
+      type: e.target.value,
+    });
+  }
+
+  onChangeDuration(e) {
+    this.setState({
+      duration: e.target.value,
+    });
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date: date,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const exercise = {
+      username: this.state.username,
+      type: this.state.type,
+      weight: this.state.weight,
+      sets: this.state.sets,
+      reps: this.state.reps,
+      duration: this.state.duration,
+      distance: this.state.distance,
+      date: this.state.date,
+    };
+
+    console.log(exercise);
+
+    axios
+      .post(
+        "http://localhost:3000/exercises/update/" + this.props.match.params.id,
+        exercise
+      )
+      .then((res) => console.log(res.data));
+
+    window.location = "/";
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Edit Exercise Log</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <select
+              ref="userInput"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            >
+              {this.state.users.map(function (user) {
+                return (
+                  <option key={user} value={user}>
+                    {user}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Type: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.type}
+              onChange={this.onChangeType}
+            />
+          </div>
+          <div className="form-group">
+            <label>Weight (in lbs): </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.weight}
+              onChange={this.onChangeWeight}
+            />
+          </div>
+          <div className="form-group">
+            <label>Sets: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.sets}
+              onChange={this.onChangeSets}
+            />
+          </div>
+          <div className="form-group">
+            <label>Reps: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.reps}
+              onChange={this.onChangeReps}
+            />
+          </div>
+          <div className="form-group">
+            <label>Duration (in minutes): </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.duration}
+              onChange={this.onChangeDuration}
+            />
+          </div>
+          <div className="form-group">
+            <label>Distance (in miles): </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.distance}
+              onChange={this.onChangeDistance}
+            />
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
+            <div>
+              <DatePicker
+                selected={this.state.date}
+                onChange={this.onChangeDate}
+              />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Edit Exercise Log"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const Exercise = (props) => (
+  <tr>
+    <td>{props.exercise.username}</td>
+    <td>{props.exercise.type}</td>
+    <td>{props.exercise.weight}</td>
+    <td>{props.exercise.sets}</td>
+    <td>{props.exercise.reps}</td>
+    <td>{props.exercise.duration}</td>
+    <td>{props.exercise.distance}</td>
+    <td>{props.exercise.date.substring(0, 10)}</td>
+    <td>
+      <Link to={"/edit/" + props.exercise._id}>edit</Link> |{" "}
+      <a
+        href="#"
+        onClick={() => {
+          props.deleteExercise(props.exercise._id);
+        }}
+      >
+        delete
+      </a>
+    </td>
+  </tr>
+);
+
+export default class ExercisesList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.deleteExercise = this.deleteExercise.bind(this);
+
+    this.state = { exercises: [] };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3000/exercises/")
+      .then((response) => {
+        this.setState({ exercises: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  deleteExercise(id) {
+    axios.delete("http://localhost:3000/exercises/" + id).then((response) => {
+      console.log(response.data);
+    });
+
+    this.setState({
+      exercises: this.state.exercises.filter((el) => el._id !== id),
+    });
+  }
+
+  exerciseList() {
+    return this.state.exercises.map((currentexercise) => {
+      return (
+        <Exercise
+          exercise={currentexercise}
+          deleteExercise={this.deleteExercise}
+          key={currentexercise._id}
+        />
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Workouts</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Username</th>
+              <th>Type</th>
+              <th>Weight</th>
+              <th>Sets</th>
+              <th>Reps</th>
+              <th>Duration</th>
+              <th>Distance</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>{this.exerciseList()}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
