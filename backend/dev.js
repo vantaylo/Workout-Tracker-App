cde1897680cf6445d56032c27b67df1257b5e90b
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-09-08 22:25:08 -0500

    heroku deployment

---
+++ backend/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://vantaylo:DIXoj9HSik3k4bSK@workouttrackerapp-shard-00-00.akvz5.gcp.mongodb.net:27017,workouttrackerapp-shard-00-01.akvz5.gcp.mongodb.net:27017,workouttrackerapp-shard-00-02.akvz5.gcp.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-u17fvz-shard-0&authSource=admin&retryWrites=true&w=majority",
+};
---
+++ build/precache-manifest.c1998209385ecf424a5a99609f92d2b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4912361f4e998ed1fcbdf255cd30c450",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc8eaec82cc7808b34d5",
+    "url": "/static/css/2.b56a0318.chunk.css"
+  },
+  {
+    "revision": "bc8eaec82cc7808b34d5",
+    "url": "/static/js/2.ea714445.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.ea714445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "742876d144a0260e7432",
+    "url": "/static/js/main.40455a72.chunk.js"
+  },
+  {
+    "revision": "135a8539c9c3beca5af3",
+    "url": "/static/js/runtime-main.879f992e.js"
+  }
+]);
